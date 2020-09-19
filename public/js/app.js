535355a5b2eaa9e490a33176d12a59a09a8b6760
Ramon Toth <ramon@Ramons-iMac.local>
2020-09-19 19:47:34 -0400

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,15 @@
-// console.log('client side javascript file is loaded')
-
-
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-// fetch(url).then((response) =>{
-//     response.json().then((data) => {
-        
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data)
-//         }
-//     })
-// })
 
 const weartherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'from javascript'
 
 
 
 weartherForm.addEventListener('submit', (e)=> {
     e.preventDefault()
     const location = search.value
-    // console.log(location)
 
 
     if (location) {
@@ -43,7 +21,6 @@ weartherForm.addEventListener('submit', (e)=> {
                 
                 if (data.error) {
                     messageOne.textContent = data.error
-                    // console.log(data.error)
                 } else {
                     messageOne.textContent = data.location
                     messageTwo.textContent = data.forecast.description + `. It is currently ${data.forecast.temp} degrees out. It feels like ${data.forecast.feelslike}. There is a ${data.forecast.precip*100}% chance of rain.`
@@ -55,7 +32,6 @@ weartherForm.addEventListener('submit', (e)=> {
     }
     else {
         messageOne.textContent = 'No Location Provided'
-        // console.log('No location provided')
     }
     
 
